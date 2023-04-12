5b877aa40134a63ff215a3b148c40cf376dd2625
Anton Gribov <gribov_an@astralnalog.ru>
2023-04-12 16:17:38 +0300

    Переименован метод добавления клиента согласно общему принципу. Добавлена возможно конфигурации HttpClient

--- Smev3Client/Extensions/ServiceCollectionExtensions.cs
+++ Smev3Client/Extensions/ServiceCollectionExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Net.Http;
 using System.Collections.Generic;
-
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,22 +8,26 @@ namespace Smev3Client.Extensions
 {
     public static class ServiceCollectionExtensions
     {
+        [Obsolete("Use AddSmev3Client instead")]
         public static void UseSmev3Client(this IServiceCollection serviceCollection)
         {
-            serviceCollection.AddHttpClient("SmevClient", (serviceProvider, httpClient) =>
+            serviceCollection.AddSmev3Client();
+        }
+
+        public static void AddSmev3Client(this IServiceCollection serviceCollection, Action<IHttpClientBuilder> httpClientBuilderConfigure = null)
+        {
+            var httpClientBuilder = serviceCollection.AddHttpClient("SmevClient", (serviceProvider, httpClient) =>
             {
                 var config = serviceProvider.GetRequiredService<IConfiguration>();
-
                 httpClient.BaseAddress = new Uri(config["Smev:Url"]);
             });
+            httpClientBuilderConfigure?.Invoke(httpClientBuilder);
 
             serviceCollection.AddSingleton<ISmev3ClientFactory>((serviceProvider) =>
             {
                 var config = serviceProvider.GetRequiredService<IConfiguration>();
-
                 var servicesConfigs = config.GetSection("Smev:Services")
                                                 .Get<Dictionary<string, SmevServiceConfig>>();
-
                 var httpClientFactory = serviceProvider
                                                 .GetRequiredService<IHttpClientFactory>();
 
